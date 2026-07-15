e880d47db21f4798dbc27db4f1cfc487b3681b2f
Rasmitha kankati <rasmithadev@gmail.com>
2026-07-15 22:12:12 +0530

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,9 @@
+function sum(n){
+    let total=0;
+    for(let i=1;i<=n;i++){
+        total +=i;
+
+    }
+    return total;
+}
+console.log(sum(10));
---
+++ vowel.js
@@ -0,0 +1,13 @@
+function vowel(str) {
+    let count=0;
+    for(let i=0;i<=str.length;i++ ) {
+        let ch=str[i];
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+            count++;
+        }
+    }
+    return count;
+
+
+}
+console.log(vowel("sky"));
