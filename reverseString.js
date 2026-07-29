5dbf3cb94fcf13641db1ed7248113cad8c01c736
Rasmitha kankati <rasmithadev@gmail.com>
2026-07-29 07:12:28 +0530

    Add files via upload

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+function reverseStr(str) {
+    return str.split(" ").reverse().join(" ");
+
+}
+console.log(reverseStr("i am good girl"));
+console.log(reverseStr("javascript is fun"));
+console.log(reverseStr("hello world"));
+
